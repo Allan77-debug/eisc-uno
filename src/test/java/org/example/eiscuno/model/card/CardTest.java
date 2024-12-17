7dbb4b007768af0eb1dda93306634cfbbe7e3bda
= <=>
2024-12-17 15:16:55 -0500

    Creación de las 3 clases test

---
+++ src/test/java/org/example/eiscuno/model/card/CardTest.java
@@ -0,0 +1,32 @@
+package org.example.eiscuno.model.card;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+
+    @Test
+    void isSpecial() {
+    }
+
+    @Test
+    void setColor() {
+    }
+
+    @Test
+    void getCard() {
+    }
+
+    @Test
+    void getImage() {
+    }
+
+    @Test
+    void getValue() {
+    }
+
+    @Test
+    void getColor() {
+    }
+}
